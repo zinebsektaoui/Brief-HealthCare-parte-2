7a1ae269aac41dd87e5d1df288c40c94bd34fe5a
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-01-28 16:19:33 +0100

    Adding some functions

--- script.js
+++ script.js
@@ -1,61 +1,108 @@
-let requests = []
-let count = 1
+let requests = [];
+let count = 1;
+let formAlerts = document.getElementsByClassName("form-alerts")[0];
+let tableAlerts = document.getElementsByClassName("table-alerts")[0];
+const allInputs = document.querySelectorAll("input"); // get all inputs
+let arr = Array.from(allInputs); // convert to an array
 
-document.querySelector("form").addEventListener("submit", addRequest)
+document.querySelector("form").addEventListener("submit", addRequest);
 
-function addRequest(e){
-    e.preventDefault()
-    const fName = document.getElementById("fName").value
-    const sName = document.getElementById("sName").value
-    const tl = document.getElementById("tl").value
-    const mail = document.getElementById("mail").value
-    const motif = document.getElementById("motif").value
-    const date = document.getElementById("date").value
-    
-    if(fName === "" || sName === "" || tl === "" || mail === "" || motif === "" || date === "") {
-        // alert("vous devez remplir tous les champs !!")
-        const allInputs = document.querySelectorAll("input")
-        let arr = Array.from(allInputs)
-        arr.forEach(input => {
-            if(input.value === ""){
-                input.classList.add("red")
-            }
-        })
-        let alert = document.getElementsByClassName("form-alerts")[0]
-        alert.innerHTML = `
-            <div class="alert alert-warning" role="alert">
-                <p>Veuillez remplir tous les champs avant de soumettre le formulaire !</p>
-            </div>
-        `
-        return;
+function addRequest(e) {
+  e.preventDefault();
+  const fName = document.getElementById("fName").value;
+  const sName = document.getElementById("sName").value;
+  const tl = document.getElementById("tl").value;
+  const mail = document.getElementById("mail").value;
+  const motif = document.getElementById("motif").value;
+  const date = document.getElementById("date").value;
+
+  if (
+    fName === "" ||
+    sName === "" ||
+    tl === "" ||
+    mail === "" ||
+    motif === "" ||
+    date === ""
+  ) {
+    arr.forEach((input) => {
+      if (input.value === "") {
+        input.classList.add("red");
+      }
+    });
+    formAlerts.innerHTML =
+      "Veuillez remplir tous les champs avant de soumettre le formulaire !";
+    formAlerts.classList.add("danger");
+    return;
+    } else {
+        const request = {
+        id: count++,
+        fName: fName,
+        sName: sName,
+        tl: tl,
+        mail: mail,
+        motif: motif,
+        date: date,
+        };
+        requests.push(request);
+        console.log(requests);
+        formAlerts.innerHTML = "Demande bien ajouté :)";
+        formAlerts.classList.add("success");
+        arr.forEach((input) => {
+        if (input !== "") {
+            input.classList.remove("red");
+        }
+        });
+        displayAll();
+        document.getElementById("addRequestForm").reset();
     }
-    const request = {
-        id : count++,
-        fName : fName,
-        sName : sName,
-        tl : tl,
-        mail : mail,
-        motif : motif,
-        date : date
-    }
-    requests.push(request)
-    console.log(requests);
-    displayAll()
+setTimeout(() => {
+  formAlerts.style.display = "none";
+}, 3000);
 }
 
-
-function displayAll(){
-    let tbody = document.querySelector("tbody")
-    tbody.innerHTML = ""
-    requests.forEach(request => {
-        let tr = document.createElement("tr")
-        tr.innerHTML = `
+function displayAll() {
+  let tbody = document.querySelector("tbody");
+  tbody.innerHTML = "";
+  requests.forEach((request) => {
+    let tr = document.createElement("tr");
+    tr.innerHTML = `
             <td>${request.fName} ${request.sName}</td>
             <td>${request.tl}</td>
             <td>${request.date}</td>
             <td>${request.motif}</td>
-            <td><button class="delete-btn"><i class="fa-solid fa-trash-can" style="color: #a30d11;"></i></button></td>
-        `
-        tbody.appendChild(tr)
-    })
+            <td><i onClick="drop(${request.id})" class="fa-solid fa-trash-can" style="color: #a30d11;"></i></td>
+        `;
+    tbody.appendChild(tr);
+  });
+}
+
+function drop(id) {
+  let request = requests.findIndex((request) => request.id === id);
+  requests.splice(request, 1);
+  let tbody = document.querySelector("tbody");
+  tbody.innerHTML = "";
+  requests.forEach((request) => {
+    let row = document.createElement("tr");
+    row.innerHTML = `
+            <td>${request.fName} ${request.sName}</td>
+            <td>${request.tl}</td>
+            <td>${request.date}</td>
+            <td>${request.motif}</td>
+            <td><i onClick="drop(${request.id})" class="fa-solid fa-trash-can" style="color: #a30d11;"></i></td>
+        `;
+    tbody.appendChild(row);
+  });
+  tableAlerts.innerHTML = "Demande bien supprimé :)";
+  tableAlerts.classList.add("success");
+  console.log(requests);
+  setTimeout(() => {
+    tableAlerts.style.display = "none";
+  }, 3000);
+}
+
+
+function search(){
+    let inputValue = document.getElementsByTagName("button")[0].value
+    let result = requests.filter(request => request.motif === inputValue)
+    console.log(result);
 }
